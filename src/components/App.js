66f90e827b72f196acc78755fbfa9bbf59edf7d9
Samuel Lakra <samuellakra1290@gmail.com>
2023-04-14 17:01:41 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import '../styles/App.css';
-
+import { signUpFormValidation } from '../utils/validation';
 
 const App = () => {
   
   return (
+    <div>
+      <form>
+      <label htmlFor='name'>Name</label>
+      <input id='name' type='text'></input><br></br>
 
+      <label htmlFor='email'>Email</label>
+      <input id='email' type='text'></input><br></br>
+
+      <label htmlFor='password'>Password</label>
+      <input id='password' type='password'></input><br></br>
+
+      <label htmlFor='consent'>Consent</label>
+      <input id='consent' type='checkbox'></input><br></br>
+
+      <button type='submit'>Signup</button>
+      </form>
+    </div>
   )
 }
 
