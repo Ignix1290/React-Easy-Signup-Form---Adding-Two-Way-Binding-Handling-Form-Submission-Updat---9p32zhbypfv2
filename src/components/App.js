62f52c39db8d469c8e82b66fd95b4a0a17db02e8
Samuel Lakra <samuellakra1290@gmail.com>
2023-04-17 19:08:26 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../styles/App.css';
 import { signUpFormValidation } from '../utils/validation';
-
 const App = () => {
-  
+  const [error, setError] = useState(null);
+  const name = useRef();
+  const email = useRef();
+  const password = useRef();
+  function validate() {
+    let data = {
+      name: name.current.value,
+      email: email.current.value,
+      password: password.current.value
+    }
+    let errorObj = signUpFormValidation(data);
+    console.log(errorObj);
+    setError(errorObj);
+  }
   return (
     <div>
       <form>
       <label htmlFor='name'>Name</label>
-      <input id='name' type='text'></input><br></br>
-
+      <input ref={name} id='name' type='text'></input><br></br>
+      { error !== null && error.name !== undefined && <div className='error'>{error.name}</div>}
       <label htmlFor='email'>Email</label>
-      <input id='email' type='text'></input><br></br>
-
+      <input ref={email} id='email' type='text'></input><br></br>
+      { error !== null && error.email !== undefined && <div className='error'>{error.email}</div>}
       <label htmlFor='password'>Password</label>
-      <input id='password' type='password'></input><br></br>
-
+      <input ref={password} id='password' type='password'></input><br></br>
+      { error !== null && error.password !== undefined && <div className='error'>{error.password}</div>}
       <label htmlFor='consent'>Consent</label>
       <input id='consent' type='checkbox'></input><br></br>
-
-      <button type='submit'>Signup</button>
+      <button onClick={validate} type='button'>Signup</button>
       </form>
     </div>
   )
 }
-
-
 export default App;
